874eefd8bcda51b73d68b87d45164cbd8a32e184
sarahzac734 <sarahzachariah13@gmail.com>
2025-08-12 20:19:36 -0400

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,38 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div>
+      <div className="">
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+       <div className="mt-40 w-200 h-140 pt-10 mx-auto  text-black rounded-3xl bg-stone-100">
+          <img className="rounded-2xl mx-auto" src="/sarah.jpg" width={200} height={20}/>
+          <p className="mt-4 text-center font-bold text-4xl">Hi I'm Sarah!</p>
+          <p className="text-center text-xl font-extrabold mt-4" >I am a Computer Science Student at the University of Maryland, College Park. <br/> I am a passionate student who is eager to learn and interested in the fields of: Full Stack Web - Development, Machine Learning, and Data Science  </p>   
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        </div>  
+        
+        <div className="mt-25 text-center font-bold text-4xl">PROJECTS</div>
+        <div className="mt-25 grid grid-cols-2">
+            <div className="mx-auto w-80 h-80 lg:w-120 lg:h-100 rounded-3xl bg-stone-100">
+            <div className="text-black text-center font-bold text-xl px-10 pt-10">Developing a time sheet simulator which uses the tree data structure to store employees with their timesheet information  </div>
+            <div className="text-black  font-extrabold px-10 pt-10">Developed using: Java, Threads </div>
+            </div>
+
+            <div className="mx-auto w-80 h-80 lg:w-120 lg:h-100 rounded-3xl bg-stone-100">
+             <div className="text-black text-center font-bold text-xl px-10 pt-10">Developing a salary predictor  </div>
+              <div className="text-black  font-extrabold px-10 pt-10">Developed using: Python, Pandas ... </div>
+            </div>
+        </div>
+
+        <Link href="https://github.com/sarahzac734/church-website" >
+        <div className="mt-25 mx-auto w-100 h-80 lg:w-290 lg:h-100 rounded-3xl bg-stone-100" >
+              <div className="text-black text-center font-bold lg:text-2xl pt-10 lg:px-50 lg:pt-30">Developing a full-stack website for St. John the Baptist Knanaya Church of Greater Washington. Provides current and future church members information about church services, events, and location.  </div>
+              <div className="text-black lg:text-xl font-extrabold pt-5 px-10 lg:px-50 lg:pt-10">Developed using: ReactJS, Next.js, React Email and Resend, AOS, Embla Carousel, Yup Validation, Tailwind CSS, MongoDB </div>
+        </div>
+        </Link>
+
     </div>
   );
 }
